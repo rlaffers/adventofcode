82a07d9da6ed7e27a2914067e4dc82c106f2d8cd
Richard Laffers <rlaffers@gmail.com>
2018-12-04 22:53:22 +0100

    Optimize performace of 3rd puzzle solution

--- 2018/3.js
+++ 2018/3.js
@@ -1,4 +1,4 @@
-import { curry, compose, map, lt, filter, reduce, length, pickBy, keys, head, add, intersection } from 'ramda'
+import { curry, compose, converge, map, lt, filter, reduce, length, pickBy, keys, head, add, intersection } from 'ramda'
 import { readFileSync } from 'fs'
 // square is defined as [x, y, a, b]
 const strToClaim = str => {
@@ -7,8 +7,7 @@ const strToClaim = str => {
 }
 const input = readFileSync('./3_input').toString().split('\n').slice(0, -1).map(strToClaim)
 
-// ==========================
-// count overlapped squares
+// PART1 ==========================
 // each square represented as string 'x,y' at top-left corner
 const getSquares = ([xStart, yStart, width, height]) => {
   const xEnd = xStart + width
@@ -35,27 +34,26 @@ const countSquareOverlaps = (squares, claim) => {
   return squares
 }
 
-// const solution1 = input.reduce(countSquareOverlaps, squareOverlaps).filter(gt(1))
 const solution1 = compose(
+  console.log,
   length,
   filter(lt(1)),
   Object.values,
   reduce(countSquareOverlaps, {}),
 )
 
-console.log('Overlapped squares=', solution1(input))
+solution1(input)
 
-// part 2 =======================================
+// PART 2 =======================================
 const getOverlappedSquares = compose(
-  Object.keys,
   pickBy(lt(1)),
   reduce(countSquareOverlaps, {}),
 )
 
-// TODO optimize performance: we run this 1200 times, arr1 has 100k items, each arr2 has hundreds of items
-const noOverlap = arr1 => arr2 => arr2.every(x => !arr1.includes(x))
+const noOverlap = obj => arr2 => arr2.every(x => obj[x] === undefined)
 
 const solution2 = compose(
+  console.log,
   add(1),  // IDs are 1-based
   head,
   keys,
@@ -63,5 +61,5 @@ const solution2 = compose(
   curry(Object.assign)({}), // convert array to object
   map(getSquares),
 )
-console.log('Non-overlapping claim=', solution2(input))
 
+solution2(input)
