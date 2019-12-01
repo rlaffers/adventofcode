f8d78aa39c69310ab355e59e0ae790f2a95c342c
Richard Laffers <rlaffers@gmail.com>
2019-12-01 14:54:22 +0100

    Add solution 1/2019

---
+++ 2019/1.js
@@ -0,0 +1,25 @@
+import { compose, flip, divide, subtract, useWith, add, identity, reduce } from 'ramda'
+import { run, readInput } from '../common'
+
+const input = readInput('./1_input').map(Number)
+
+
+// PART 1
+const massToFuel = compose(
+  flip(subtract)(2),
+  Math.floor,
+  flip(divide)(3),
+)
+
+const solution1 = reduce(useWith(add, [identity, massToFuel]), 0)
+run('PART1', solution1, input)
+
+// PART 2
+const massToFuelRecursive = (mass) => {
+  const fuel = massToFuel(mass)
+  return fuel <= 0 ? 0 : fuel + massToFuelRecursive(fuel)
+}
+
+const solution2 = reduce(useWith(add, [identity, massToFuelRecursive]), 0)
+run('PART2', solution2, input)
+
