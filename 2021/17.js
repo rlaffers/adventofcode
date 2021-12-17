5a3eccb99b14c828e8092dfa4dbc9544e495ccd9
Richard Laffers <rlaffers@gmail.com>
2021-12-18 00:01:37 +0100

    Prettify 17/2021

--- 2021/17.js
+++ 2021/17.js
@@ -3,23 +3,23 @@ import S from 'sanctuary'
 
 const maxUpwardSpeed = ({ y1, y2 }) => Math.abs(y1) - 1
 
-const speedToTravelDistance = (n) => (n * (n + 1)) / 2
+const speedToDistance = (n) => (n * (n + 1)) / 2
 
 // PART 1
-const solver1 = S.pipe([maxUpwardSpeed, speedToTravelDistance])
+const solver1 = S.pipe([maxUpwardSpeed, speedToDistance])
 
 // --------------------------------------------------------------------
 // PART 2
-const findXSpeedLimits = ({ x1, x2 }) => {
+const xSpeedRange = ({ x1, x2 }) => {
   const max = x2 + 1
   let min = 0
-  while (speedToTravelDistance(min) < x1) {
+  while (speedToDistance(min) < x1) {
     min += 1
   }
   return [min, max]
 }
 
-const findYSpeedLimits = ({ y1, y2 }) => {
+const ySpeedRange = ({ y1, y2 }) => {
   const min = -1 * Math.abs(y1) // max downward speed
   const max = maxUpwardSpeed({ y1, y2 })
   return [min, max]
@@ -55,9 +55,7 @@ const tryAllSpeeds =
   }
 
 // S.ap is the S combinator: S = f => g => f(x)(g(x))
-const solver2 = S.ap(tryAllSpeeds)(
-  S.lift2(R.pair)(findXSpeedLimits)(findYSpeedLimits),
-)
+const solver2 = S.ap(tryAllSpeeds)(S.lift2(R.pair)(xSpeedRange)(ySpeedRange))
 
 export const solvers = [solver1, solver2]
 export const parser = (text) => {
