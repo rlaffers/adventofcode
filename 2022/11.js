b61d2cf48ee77f20c1a58fe08d1c7fdab733bf73
Richard Laffers <rlaffers@gmail.com>
2022-12-12 07:55:54 +0100

    wip solution 11/2022 second part

--- 2022/11.js
+++ 2022/11.js
@@ -1,29 +1,37 @@
 import * as R from 'ramda'
 import S from 'sanctuary'
+import Big from 'big.js'
+
+Big.RM = Big.roundDown
 
 function parseMonkey(lines) {
   const divisor = Number(R.match(/divisible\sby\s(\d+)/, lines[3])[1])
   const targetTrue = Number(R.match(/monkey\s(\d+)/, lines[4])[1])
   const targetFalse = Number(R.match(/monkey\s(\d+)/, lines[5])[1])
 
-  const [, , operator, operand] = R.match(/=\s(\w+)\s(.)\s(\w+)/, lines[2])
-  const op = eval(`old => old ${operator} BigInt(${operand})`)
+  const operation = R.match(/=\s(\w+)\s(.)\s(\w+)/, lines[2])
+
+  let op
+  if (operation[2] === '+') {
+    op = operation[3] === 'old' ? (x) => x.plus(x) : (x) => x.plus(operation[3])
+  } else {
+    op =
+      operation[3] === 'old' ? (x) => x.times(x) : (x) => x.times(operation[3])
+  }
 
   return {
-    items: R.match(/(\d+)/g, lines[1]).map(BigInt),
-    // op: eval(`old => ${R.match(/=\s(.+)/, lines[2])[1]}`),
+    items: R.match(/(\d+)/g, lines[1]).map(Big),
     op,
-    target: eval(
-      `x => x % ${divisor}n === 0n ? ${targetTrue} : ${targetFalse}`,
-    ),
+    target: (x) => (x.mod(divisor).eq(0) ? targetTrue : targetFalse),
     inspections: 0,
   }
 }
 
 const play =
-  (rounds, relief = R.divide(R.__, 3n)) =>
+  (rounds, relief = (x) => x.div(3).round()) =>
   (monkeys) => {
     for (let r = 1; r <= rounds; r++) {
+      console.log(`ROUND ${r}`) // TODO remove console.log
       for (let monkey of monkeys) {
         for (let item of monkey.items) {
           const nextValue = relief(monkey.op(item))
@@ -54,11 +62,11 @@ const solver2 = S.pipe([
   R.filter(Boolean),
   R.splitEvery(6),
   R.map(parseMonkey),
-  play(10000, R.identity),
+  play(1000, R.identity),
   R.map(R.prop('inspections')),
-  R.sort(R.subtract),
-  R.takeLast(2),
-  R.apply(R.multiply),
+  // R.sort(R.subtract),
+  // R.takeLast(2),
+  // R.apply(R.multiply),
 ])
 
 export const solvers = [solver1, solver2]
