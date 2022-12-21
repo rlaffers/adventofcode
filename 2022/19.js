d57324b51bcb4bde21be3f3b8b0e0431fadb1a19
Richard Laffers <rlaffers@gmail.com>
2022-12-21 21:04:02 +0100

    add broken brute-force 19/2022

--- 2022/19.js
+++ 2022/19.js
@@ -1,192 +1,110 @@
 import * as R from 'ramda'
 import S from 'sanctuary'
 import colors from 'colors'
-
-function timeToNext(current1, current2, target1, target2, speed1, speed2) {
-  if (speed1 === 0 || speed2 === 0) return Infinity
-  if (current1 >= target1 && current2 >= target2) return 0
-  const need1 = Math.max(target1 - current1, 0)
-  const need2 = Math.max(target2 - current2, 0)
-  return Math.max(Math.ceil(need1 / speed1), Math.ceil(need2 / speed2))
-}
-
-function substract(materialIndex, cost1, cost2, inventory) {
-  inventory[0] -= cost1
-  inventory[materialIndex] -= cost2
-  return inventory
-}
+import { memoize } from 'lodash-es'
 
 const add = R.zipWith(R.add)
-// function add(inventory, speeds) {
-//   for (let i = 0, l = inventory.length; i < l; i++) {
-//     inventory[i] += speeds[0]
-//   }
-//   return inventory
-// }
 
-function run([, oreCost, clayCost, obsCost1, obsCost2, geoCost1, geoCost2]) {
-  const speeds = [1, 0, 0, 0]
-  let inv = [0, 0, 0, 0]
-  let timeLeft = 25
-  while (timeLeft > 1) {
-    timeLeft--
-    console.log(colors.green(`\nTime left: ${timeLeft}`))
-    console.log(colors.blue('inventory'), inv, colors.cyan('Speeds:'), speeds) // TODO remove console.log
-
-    // TODO check what can be built. If 1+ robots can be built, explore every posibility. Then choose the max benefit one
-    // if (robotsToBuild.length > 0) {
-    //   // TODO get those results and continue with the highest
-    //   run(inv, timeLeft, )
-    // }
-
-    const nextGeo = timeToNext(
-      inv[0],
-      inv[2],
-      geoCost1,
-      geoCost2,
-      speeds[0],
-      speeds[2],
-    )
-    console.log('next geode robot in', nextGeo) // TODO remove console.log
-    if (nextGeo === 0) {
-      console.log(colors.magenta('creating geo robot'))
-      inv = substract(2, geoCost1, geoCost2, inv)
-      // collect new stuff
-      inv = add(inv, speeds)
-      speeds[3]++
-      // TODO we may still be able to create more robots in this minute
-      continue
-    }
-
-    const nextObs = timeToNext(
-      inv[0],
-      inv[1],
-      obsCost1,
-      obsCost2,
-      speeds[0],
-      speeds[1],
-    )
-    console.log('next obsidian robot in', nextObs) // TODO remove console.log
-    if (nextObs === 0) {
-      // make it but only if it does not lower nextGeo
-      // if next geo takes longer than the remaining time, we need not worry about spending stuff
-      const nextGeoAfterMakingObs = timeToNext(
-        inv[0] - obsCost1,
-        inv[2],
-        geoCost1,
-        geoCost2,
-        speeds[0],
-        speeds[2],
-      )
-      if (nextGeoAfterMakingObs <= nextGeo || nextGeo > timeLeft) {
-        // make obsidian
-        console.log(colors.magenta('creating obsidian robot'))
-        inv = substract(1, obsCost1, obsCost2, inv)
-        // collect stuff
-        inv = add(inv, speeds)
-        speeds[2]++
-        // TODO we may still be able to create more robots in this minute
-        continue
-      }
-    }
-
-    const nextClay = timeToNext(
-      inv[0],
-      Infinity,
-      clayCost,
-      0,
-      speeds[0],
-      Infinity,
-    )
-    console.log('next clay robot in', nextClay) // TODO remove console.log
-    if (nextClay === 0) {
-      // make it but only if it does not lower nextGeo nor nextObs
-      const nextGeoAfterMakingClay = timeToNext(
-        inv[0] - clayCost,
-        inv[2],
-        geoCost1,
-        geoCost2,
-        speeds[0],
-        speeds[2],
-      )
-      const nextObsAfterMakingClay = timeToNext(
-        inv[0] - clayCost,
-        inv[1],
-        obsCost1,
-        obsCost2,
-        speeds[0],
-        speeds[1],
-      )
-      if (
-        (nextGeo < Infinity || nextGeoAfterMakingClay <= nextGeo) &&
-        // (nextObsAfterMakingClay <= nextObs || nextObs > timeLeft)
-        (nextObsAfterMakingClay <= nextObs || nextObs > timeLeft)
-      ) {
-        // make clay
-        console.log(colors.magenta('creating clay robot'))
-        inv = substract(0, clayCost, 0, inv)
-        // collect stuff
-        inv = add(inv, speeds)
-        speeds[1]++
-        // TODO we may still be able to create more robots in this minute
-        continue
-      }
-    }
-
-    const nextOre = timeToNext(
-      inv[0],
-      Infinity,
-      oreCost,
-      0,
-      speeds[0],
-      Infinity,
-    )
-    console.log('next ore robot in', nextOre) // TODO remove console.log
-    if (nextOre === 0) {
-      // make it but only if it does not lower nextGeo nor nextObs nor nextClay
-      const nextGeoAfterMakingOre = timeToNext(
-        inv[0] - oreCost,
-        inv[2],
-        geoCost1,
-        geoCost2,
-        speeds[0],
-        speeds[2],
-      )
-      const nextObsAfterMakingOre = timeToNext(
-        inv[0] - oreCost,
-        inv[1],
-        obsCost1,
-        obsCost2,
-        speeds[0],
-        speeds[1],
-      )
-      const nextClayAfterMakingOre = timeToNext(
-        inv[0] - oreCost,
-        Infinity,
-        clayCost,
-        0,
-        speeds[0],
-        Infinity,
-      )
-      if (
-        (nextGeo < Infinity || nextGeoAfterMakingOre <= nextGeo) &&
-        (nextObs < Infinity || nextObsAfterMakingOre <= nextObs) &&
-        (nextClayAfterMakingOre <= nextClay || nextClay > timeLeft)
-      ) {
-        // make ore
-        console.log(colors.magenta('creating ore robot'))
-        inv = substract(0, oreCost, 0, inv)
-        // collect stuff
-        inv = add(inv, speeds)
-        speeds[0]++
-        // TODO we may still be able to create more robots in this minute
-        continue
-      }
-    }
-
-    inv = add(inv, speeds)
+const pad = (n) => {
+  let str = ''
+  for (let i = 0; i < n; i++) {
+    str += ' '
   }
-  return inv
+  return str
+}
+
+function whatCanBeBuilt(store, costs) {
+  const canBuild = []
+  const [oreCost, clayCost, obsCost1, obsCost2, geoCost1, geoCost2] = costs
+  // ore
+  if (store[0] >= oreCost) canBuild.push(0)
+  // clay
+  if (store[0] >= clayCost) canBuild.push(1)
+  // obsidian
+  if (store[0] >= obsCost1 && store[1] >= obsCost2) canBuild.push(2)
+  // geode
+  if (store[0] >= geoCost1 && store[2] >= geoCost2) canBuild.push(3)
+  return canBuild
+}
+
+function buildRobot(robot, costs, store, speeds) {
+  const nextStore = [...store]
+  const nextSpeeds = [...speeds]
+  let cost1 = 0
+  let cost2 = 0
+  if (robot <= 1) {
+    cost1 = costs[robot]
+    cost2 = 0
+  } else if (robot === 2) {
+    cost1 = costs[2]
+    cost2 = costs[3]
+  } else {
+    cost1 = costs[4]
+    cost2 = costs[5]
+  }
+  nextStore[0] -= cost1
+  nextStore[robot - 1] -= cost2
+  nextSpeeds[robot]++
+  return [nextStore, nextSpeeds]
+}
+
+const simulate = memoize(_simulate, (...args) => args.join('|'))
+
+function _simulate(timeLeft, store, costs, speeds) {
+  // console.log(
+  //   // colors.green(pad(depth) + `Time left: ${timeLeft}`),
+  //   colors.green(`Time left: ${timeLeft}`),
+  //   colors.blue('Store'),
+  //   store,
+  //   colors.blue('Speeds'),
+  //   speeds,
+  // )
+
+  if (timeLeft < 1) {
+    return store
+  }
+
+  // indices of buildable robots. 0-ore, 1-clay, 2-obsidian, 3-geode
+  const buildable = whatCanBeBuilt(store, costs)
+
+  if (buildable.length > 0) {
+    let maxGeodes = store[3]
+    let bestRobotToBuild = null
+
+    const simulated = {}
+
+    for (let robot of buildable) {
+      const [nextStore, nextSpeeds] = buildRobot(robot, costs, store, speeds)
+      simulated[robot] = simulate(timeLeft - 1, nextStore, costs, nextSpeeds)
+
+      if (simulated[robot][3] > maxGeodes) {
+        maxGeodes = simulated[robot][3]
+        bestRobotToBuild = robot
+      }
+    }
+
+    // consider maxGeodes when nothing is built
+    const geodesWhenNothingBuiltHere = simulate(
+      timeLeft - 1,
+      add(store, speeds),
+      costs,
+      speeds,
+    )[3]
+    if (geodesWhenNothingBuiltHere > maxGeodes) {
+      bestRobotToBuild = null
+    }
+
+    if (bestRobotToBuild) {
+      return simulated[bestRobotToBuild]
+    }
+    // means some robots are buildable, but no fork gave any extra geodes, or not building
+    // anything gave best results
+    // return simulate(timeLeft - 1, add(store, speeds), costs, speeds)
+  }
+
+  // just let them collect stuff
+  // console.log(colors.magenta(pad(depth) + 'No robot built this round'))
+  return simulate(timeLeft - 1, add(store, speeds), costs, speeds)
 }
 
 // PART 1
@@ -195,11 +113,11 @@ const solver1 = S.pipe([
   R.filter(Boolean),
   R.map(
     R.match(
-      /(\d+):.*(\d+) ore\..*(\d+) ore\..*costs (\d+) ore and (\d+).*costs (\d+) ore and (\d+)/,
+      /(\d+) ore\..*(\d+) ore\..*costs (\d+) ore and (\d+).*costs (\d+) ore and (\d+)/,
     ),
   ),
   R.map(R.pipe(R.tail, R.map(Number))),
-  R.map(run),
+  R.map((costs) => simulate(24, [0, 0, 0, 0], costs, [1, 0, 0, 0])),
 ])
 
 // PART 2
