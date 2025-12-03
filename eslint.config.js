ce89f0a02e632d3a4280ca4190d903824c546a8f
Richard Laffers <rlaffers@gmail.com>
2025-12-03 12:39:06 +0100

    use prettier eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,11 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import { defineConfig } from 'eslint/config'
-import stylistic from '@stylistic/eslint-plugin'
+import prettier from 'eslint-config-prettier'
 
 export default defineConfig([
   js.configs.recommended,
-  stylistic.configs.customize({
-    indent: 2,
-    quotes: 'single',
-    semi: false,
-    jsx: false,
-  }),
+  prettier,
   {
     files: ['**/*.{js,mjs,cjs}'],
     languageOptions: {
@@ -20,8 +15,6 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': 0,
-      '@stylistic/brace-style': ['error', '1tbs', { allowSingleLine: true }],
-      '@stylistic/arrow-parens': ['error', 'always'],
     },
   },
 ])
