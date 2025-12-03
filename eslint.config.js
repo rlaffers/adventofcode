9bd3eecaf20a404500f7106272b622069aa2ac52
Richard Laffers <rlaffers@gmail.com>
2025-12-03 12:05:10 +0100

    upgrade eslint

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
+import stylistic from '@stylistic/eslint-plugin'
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      'no-unused-vars': 0,
+    },
+  },
+  {
+    plugins: {
+      '@stylistic': stylistic,
+    },
+  },
+])
