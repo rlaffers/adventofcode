982b8ae3cca8e0aa597347773ab109a9c5158ee0
Richard Laffers <rlaffers@gmail.com>
2025-12-06 12:34:11 +0100

    solve 6 2025

---
+++ 2025/6.js
@@ -0,0 +1,91 @@
+import * as R from 'ramda'
+import S from 'sanctuary'
+
+// PART 1
+const solver1 = S.pipe([
+  R.map(S.pipe([R.split(' '), R.filter(Boolean)])),
+  R.transpose,
+  R.map((problem) => {
+    const op = R.last(problem)
+    const numbers = S.pipe([R.init, R.map(Number)])(problem)
+    if (op === '+') return R.sum(numbers)
+    return R.reduce(R.multiply, R.head(numbers), R.tail(numbers))
+  }),
+  R.sum,
+])
+
+const splitByWidths = (widths) => (str) => {
+  const chunks = []
+  let chunk = ''
+  let wIdx = 0
+  let width = widths[wIdx]
+  for (let i = 0; i <= str.length - 1; i += 1) {
+    if (width === undefined) {
+      throw new Error(`undefined column with for col ${wIdx}`)
+    }
+    const char = str[i]
+    chunk += char
+    if (chunk.length >= width) {
+      chunks.push(chunk)
+      chunk = ''
+      wIdx += 1
+      width = widths[wIdx]
+      i += 1 // skip the next space
+    }
+  }
+  return chunks
+}
+
+function splitByColumns(lines) {
+  const widths = S.pipe([
+    //
+    R.last,
+    R.split(/[*+]/),
+    R.tail,
+    R.map(R.length),
+  ])(lines)
+
+  // correct for 1 char cut off at the end
+  const lastLen = R.last(widths)
+  widths[widths.length - 1] = lastLen + 1
+
+  const result = S.pipe([R.init, R.map(splitByWidths(widths))])(lines)
+
+  const ops = S.pipe([R.last, R.split(' '), R.filter(Boolean)])(lines)
+  result.push(ops)
+
+  return result
+}
+
+function doCephaloMath(problem) {
+  const op = R.last(problem)
+  const cols = R.init(problem)
+  let idx = 0
+  let maxIdx = undefined
+  const cephaloNums = []
+  while (idx <= maxIdx || maxIdx === undefined) {
+    let num = ''
+    for (const col of cols) {
+      maxIdx = Math.max(col.length - 1, maxIdx ?? 0)
+      const char = col[idx]
+      if (char !== ' ') {
+        num += char
+      }
+    }
+    cephaloNums.push(Number(num))
+    idx += 1
+  }
+  if (op === '+') return R.sum(cephaloNums)
+  return R.reduce(R.multiply, 1, cephaloNums)
+}
+
+// PART 2
+const solver2 = S.pipe([
+  // your code here
+  splitByColumns,
+  R.transpose,
+  R.map(doCephaloMath),
+  R.sum,
+])
+
+export const solvers = [solver1, solver2]
